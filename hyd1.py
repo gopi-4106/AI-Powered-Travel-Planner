be0b97d234dcbb58873a507a08eb97582e987d60
gopi-4106 <gopikrishna4106@gmail.com>
2025-02-27 13:29:44 +0530

    Update hyd1.py

--- hyd1.py
+++ hyd1.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import langchain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.output_parsers import StrOutputParser
